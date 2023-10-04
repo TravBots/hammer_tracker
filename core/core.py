1d39edd6f0f563c2ba55a81178d0226df199c6a5
Jack Bonatakis <jackbonatakis@gmail.com>
2023-10-03 22:15:27 -0400

    move cfd modal to interactions dir

--- core/core.py
+++ core/core.py
@@ -14,7 +14,7 @@ from utils.validators import *
 
 from factory import AppFactory
 
-import traceback
+from interactions.cfd import Cfd
 
 intents = discord.Intents.all()
 intents.message_content = True
@@ -277,63 +277,6 @@ if __name__ == "__main__":
             value=f"Expected hammer size (in crop consumption): **{int(hammer_size):,}**",
         )
         await interaction.response.send_message(embed=embed)
-
-    class Cfd(discord.ui.Modal, title="CFD"):
-        cfd_title = discord.ui.TextInput(
-            label="CFD Title",
-            placeholder="Who are we defending?",
-        )
-
-        coordinates = discord.ui.TextInput(
-            label="Coordinates",
-            placeholder="x|y or x/y",
-        )
-
-        land_time = discord.ui.TextInput(
-            label="Land Time",
-            placeholder="yy/mm/dd hh:mm:ss, e.g. 23/04/20",
-        )
-
-        amount_requested = discord.ui.TextInput(
-            label="Amount Requested",
-            placeholder="How much defense do we need?",
-        )
-
-        notes = discord.ui.TextInput(
-            label="Notes",
-            style=discord.TextStyle.long,
-            placeholder="Leave any other relevant information here",
-            required=False,
-        )
-
-        async def on_submit(self, interaction: discord.Interaction):
-            embed = discord.Embed(color=Colors.SUCCESS)
-            embed.add_field(name="Title", value=f"{self.cfd_title.value}", inline=False)
-            embed.add_field(
-                name="Coordinates", value=f"{self.coordinates.value}", inline=False
-            )
-            embed.add_field(
-                name="Land Time", value=f"{self.land_time.value}", inline=False
-            )
-            embed.add_field(
-                name="Defense Requested",
-                value=f"{self.amount_requested.value}",
-                inline=False,
-            )
-            embed.add_field(name="Notes", value=f"{self.notes.value}", inline=False)
-            await interaction.response.send_message(
-                embed=embed,
-                ephemeral=False,
-            )
-
-        async def on_error(
-            self, interaction: discord.Interaction, error: Exception
-        ) -> None:
-            await interaction.response.send_message(
-                "Oops! Something went wrong.", ephemeral=True
-            )
-
-            traceback.print_exception(type(error), error, error.__traceback__)
 
     @client.tree.command(description="Submit a new CFD")
     async def cfd(interaction: discord.Interaction):
