1c3b02ddd4b50f8bf83794e8ff66565b79840df8
jbonatakis <jbonatakis@users.noreply.github.com>
2023-10-10 22:11:34 -0400

    Add python version of load.sh (#10)
    * Add python version of load.sh
    
    * FIx hashbang

---
+++ dash_site/load.py
@@ -0,0 +1,38 @@
+#!/projects/hammer_tracker/dash_site/env/bin/python3
+import requests
+import sqlite3
+
+from servers import SERVER_LINKS
+
+
+with open("sql/replace_x_world.sql", "r") as sql_file:
+    replace_x_world = sql_file.read()
+
+with open("sql/insert_map_history.sql", "r") as sql_file:
+    insert_map_history = sql_file.read()
+
+for server in SERVER_LINKS:
+    server_link = server[0]
+    server_nick = server[1]
+
+    try:
+        print(f"Loading {server_link} into x_world")
+        data = requests.get(server_link + "/map.sql")
+        cnx = sqlite3.connect(f"../databases/{server_nick}.db")
+        cnx.executescript(replace_x_world)
+
+        records = 0
+        for record in data.text.split("\n"):
+            cnx.execute(record)
+            records += 1
+        print(f"Loaded {records} into {server_nick}.db")
+        cnx.commit()
+
+        print(f"Updating history table for {server_link}\n")
+        cnx.executescript(insert_map_history)
+        cnx.commit()
+
+        cnx.close()
+
+    except Exception as e:
+        print(f"Failed to collect map data for {server_link}\n{e}")
---
+++ dash_site/servers.py
@@ -0,0 +1,5 @@
+SERVER_LINKS = [
+    ("https://ts3.x1.america.travian.com", "am3"),
+    ("https://ts6.x1.international.travian.com", "int6"),
+    ("https://ts6.x1.europe.travian.com", "eu6"),
+]
