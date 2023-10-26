cc0c848ef4378d815b60f9596d62e61867d52035
Jack Bonatakis <jackbonatakis@gmail.com>
2023-10-25 20:55:43 -0400

    Add three day pop view

---
+++ databases/game_servers/views/v_three_day_pop.sql
@@ -0,0 +1,25 @@
+with lags as(
+    select 
+        date,
+        player_id,
+        player_name, 
+        village_id,
+        village_name,
+        population as today, 
+        coalesce(lag(population, 1) over(partition by village_id order by date),0) as yesterday,
+        coalesce(lag(population, 2) over(partition by village_id order by date),0) as two_days_ago,
+        coalesce(lag(population, 3) over(partition by village_id order by date),0) as three_days_ago
+    from v_map_history 
+    group by 1, 2, 3, 4, 5
+)
+select
+    player_id,
+    player_name,
+    village_id,
+    village_name,
+    today - yesterday as one_day_diff,
+    today - two_days_ago as two_day_diff,
+    today - three_days_ago as three_day_diff
+from lags 
+where date = current_date
+;
