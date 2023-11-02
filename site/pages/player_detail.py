b231f14636c1996f844c41f72cd363e879dac75b
Duncan McConnell <ddmcconnell4@gmail.com>
2023-11-02 14:56:23 -0500

    Revamp Pop Table To Seven Days (#17)
    * Site view updates
    
    * Drop three_day_pop view
    
    * Fix improper type in df warnings

--- site/pages/player_detail.py
+++ site/pages/player_detail.py
@@ -6,15 +6,20 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 from dash import dcc, html, dash_table
+from datetime import datetime, timedelta
 
 dash.register_page(__name__, path_template="/players/<player_id>")
 
 
 def pop_table(player_id, cnx):
-    village_pop_query = f"select * from v_three_day_pop where player_id = {player_id};"
+    village_pop_query = f"select * from v_seven_day_pop where player_id = {player_id};"
     print(village_pop_query)
 
     df = pd.read_sql_query(village_pop_query, cnx)
+    working_date = datetime.strptime(df["load_date"][0], "%Y-%m-%d")
+
+    add_pop_diff_markdown(df)
+
     pop_table = dash_table.DataTable(
         columns=[
             {
@@ -30,46 +35,63 @@ def pop_table(player_id, cnx):
                 "presentation": "markdown",
             },
             {
-                "name": "Current Population",
-                "id": "current_pop",
+                "name": (working_date).strftime("%m-%d"),
+                "id": "today",
                 "type": "numeric",
                 "presentation": "markdown",
             },
             {
-                "name": "One Day Diff",
-                "id": "one_day_diff",
+                "name": (working_date - timedelta(days=1)).strftime("%m-%d"),
+                "id": "yesterday",
                 "type": "numeric",
                 "presentation": "markdown",
             },
             {
-                "name": "Two Day Diff",
-                "id": "two_day_diff",
+                "name": (working_date - timedelta(days=2)).strftime("%m-%d"),
+                "id": "two_days_ago",
                 "type": "numeric",
                 "presentation": "markdown",
             },
             {
-                "name": "Three Day Diff",
-                "id": "three_day_diff",
+                "name": (working_date - timedelta(days=3)).strftime("%m-%d"),
+                "id": "three_days_ago",
+                "type": "numeric",
+                "presentation": "markdown",
+            },
+            {
+                "name": (working_date - timedelta(days=4)).strftime("%m-%d"),
+                "id": "four_days_ago",
+                "type": "numeric",
+                "presentation": "markdown",
+            },
+            {
+                "name": (working_date - timedelta(days=5)).strftime("%m-%d"),
+                "id": "five_days_ago",
+                "type": "numeric",
+                "presentation": "markdown",
+            },
+            {
+                "name": (working_date - timedelta(days=6)).strftime("%m-%d"),
+                "id": "six_days_ago",
                 "type": "numeric",
                 "presentation": "markdown",
             },
         ],
         data=df.to_dict("records"),
-        filter_action="native",
-        sort_action="native",
-        page_action="native",
-        page_current=0,
-        page_size=25,
         style_table={
             "width": "90%",
         },
         style_data={
             "width": "150px",
-            "minWidth": "150px",
-            "maxWidth": "150px",
+            "minWidth": "50px",
+            "maxWidth": "200px",
             "overflow": "hidden",
             "textOverflow": "ellipsis",
         },
+        style_cell_conditional=[
+            {"if": {"column_id": c}, "textAlign": "left"}
+            for c in ["village_name", "founded"]
+        ],
         markdown_options={"link_target": "_self", "html": True},
     )
 
@@ -165,3 +187,63 @@ def layout(player_id=None):
             ),
         ],
     )
+
+
+def add_pop_diff_markdown(df):
+    df["today"] = df["today"].astype(str)
+    df["yesterday"] = df["yesterday"].astype(str)
+    df["two_days_ago"] = df["two_days_ago"].astype(str)
+    df["three_days_ago"] = df["three_days_ago"].astype(str)
+    df["four_days_ago"] = df["four_days_ago"].astype(str)
+    df["five_days_ago"] = df["five_days_ago"].astype(str)
+
+    for index, row in df.iterrows():
+        today_markdown = generate_markdown(row["today"], row["yesterday"])
+        yesterday_markdown = generate_markdown(row["yesterday"], row["two_days_ago"])
+        two_days_ago_markdown = generate_markdown(
+            row["two_days_ago"], row["three_days_ago"]
+        )
+        three_days_ago_markdown = generate_markdown(
+            row["three_days_ago"], row["four_days_ago"]
+        )
+        four_days_ago_markdown = generate_markdown(
+            row["four_days_ago"], row["five_days_ago"]
+        )
+        five_days_ago_markdown = generate_markdown(
+            row["five_days_ago"], row["six_days_ago"]
+        )
+
+        df.at[index, "today"] = str(df.at[index, "today"]) + " " + today_markdown
+        df.at[index, "yesterday"] = (
+            str(df.at[index, "yesterday"]) + " " + yesterday_markdown
+        )
+        df.at[index, "two_days_ago"] = (
+            str(df.at[index, "two_days_ago"]) + " " + two_days_ago_markdown
+        )
+        df.at[index, "three_days_ago"] = (
+            str(df.at[index, "three_days_ago"]) + " " + three_days_ago_markdown
+        )
+        df.at[index, "four_days_ago"] = (
+            str(df.at[index, "four_days_ago"]) + " " + four_days_ago_markdown
+        )
+        df.at[index, "five_days_ago"] = (
+            str(df.at[index, "five_days_ago"]) + " " + five_days_ago_markdown
+        )
+
+
+def generate_markdown(first, second):
+    first = int(first)
+    second = int(second)
+
+    if first == 0:
+        return ""
+
+    pop_diff = first - second
+    markdown = ""
+    if pop_diff > 0:
+        markdown = f'<font color="green">(+{pop_diff})</font>'
+    elif pop_diff < 0:
+        markdown = f'<font color="red">({pop_diff})</font>'
+    else:
+        markdown = f"({pop_diff})"
+    return markdown
