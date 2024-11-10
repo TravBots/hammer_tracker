4027cf5e8dbc68cc68a8dc490195af6cd68625b8
Jack Bonatakis <jackbonatakis@gmail.com>
2024-11-10 17:42:39 +0000

    Point eu2 to eu2 db

--- databases/servers.py
+++ databases/servers.py
@@ -3,5 +3,5 @@ SERVER_LINKS = [
     ("https://ts6.x1.international.travian.com", "int6"),
     ("https://ts6.x1.europe.travian.com", "eu6"),
     ("https://ts20.x2.arabics.travian.com", "arab20"),
-    ("https://ts2.x1.europe.travian.com", "eu6"),
+    ("https://ts2.x1.europe.travian.com", "eu2"),
 ]
