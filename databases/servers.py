6bccf3e9f73eb338897e867098c211a8d4aebb47
Jack Bonatakis <jackbonatakis@gmail.com>
2024-11-10 00:06:47 +0000

    Remove am2

--- databases/servers.py
+++ databases/servers.py
@@ -3,6 +3,5 @@ SERVER_LINKS = [
     ("https://ts6.x1.international.travian.com", "int6"),
     ("https://ts6.x1.europe.travian.com", "eu6"),
     ("https://ts20.x2.arabics.travian.com", "arab20"),
-    ("https://ts2.x1.america.travian.com", "am2"),
     ("https://ts2.x1.europe.travian.com", "eu6"),
 ]
